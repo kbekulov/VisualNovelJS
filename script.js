c5310f35b03913c737c7bf4047a4d25838dfba39
Kiril Bekulov <kiril@bekulov.com>
2023-03-31 16:57:41 +0300

    sdsaf

--- script.js
+++ script.js
@@ -17,90 +17,96 @@ function fadeIn(element, duration) {
 
 fadeIn(background, 2000);
 
-// start typewriter here!
-const container = document.querySelector(".container");
+// Fetch text from txt file
+async function fetchText() {
+  const response = await fetch("text.txt");
+  const text = await response.text();
+  return text;
+}
 
-const text = `Meanwhile, Felix strode into the bustling café, inhaling deeply and savouring the rich aroma of freshly brewed coffee beans. He took his time scanning the busy establishment, looking for any signs of suspicious activity. His excessive caution was not instinctive, but merely a formality imposed on him by his work. In the corner of his eye, he noticed a young female barista working behind the counter. He proceeded to sauntered up to the counter. With a playful smile on his face he ordered his coffee. Awaiting his spicy iced latte, he stole glances at his phone, keeping an eye on the screen. His attention was soon diverted, however, as the female barista smiled at him and asked if there was anything else she could get for him.
+async function init() {
+  const text = await fetchText();
+  const paragraphs = text
+    .split("\n")
+    .filter((line) => line.trim() !== "");
 
-"Oh, just your number," Felix quipped, flashing his most charming smile.
-The barista chuckled, but shook her head. "Sorry, I'm taken."
+  // Rest of the code from the previous script.js
 
-Lynleit curiously observed Felix from a distance, as he was making a fool of himself at the counter. She couldn't believe how little regard he had for the seriousness of his job.`;
+  const container = document.querySelector(".container");
 
-const paragraphs = text
-  .split("\n")
-  .filter((line) => line.trim() !== ""); // Add this line to filter out empty lines
+  let currentParagraph = 0;
+  let index = 0;
+  let textElement;
 
-let currentParagraph = 0;
-let index = 0;
-let textElement;
+  function fadeOutPreviousParagraphs() {
+    const paragraphs = container.querySelectorAll("p");
+    paragraphs.forEach((paragraph, index) => {
+      if (index < currentParagraph) {
+        paragraph.style.opacity = "0.5";
+      }
+    });
+  }
 
-function fadeOutPreviousParagraphs() {
-  const paragraphs = container.querySelectorAll("p");
-  paragraphs.forEach((paragraph, index) => {
-    if (index < currentParagraph) {
-      paragraph.style.opacity = "0.5";
+  function clearPreviousParagraphs() {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+  }
+
+  function createParagraph() {
+    const paragraph = document.createElement("p");
+    container.appendChild(paragraph);
+    return paragraph;
+  }
+
+  let typingInProgress = false;
+
+  function typeText() {
+    textElement = createParagraph();
+    fadeOutPreviousParagraphs();
+    typingInProgress = true;
+    const typeInterval = setInterval(() => {
+      if (index < paragraphs[currentParagraph].length) {
+        const char = paragraphs[currentParagraph].charAt(index);
+        textElement.textContent += char;
+        index++;
+
+        // Check if the text overflows the container's boundaries
+        if (container.scrollHeight > container.clientHeight) {
+          clearPreviousParagraphs();
+          textElement = createParagraph();
+          textElement.textContent = char;
+        }
+      } else {
+        clearInterval(typeInterval);
+        index = 0;
+      }
+    }, 50);
+  }
+
+  typeText();
+
+  function nextParagraph() {
+    if (typingInProgress) {
+      textElement.textContent = paragraphs[currentParagraph];
+      index = paragraphs[currentParagraph].length;
+      typingInProgress = false;
+      return;
+    }
+    if (currentParagraph < paragraphs.length - 1) {
+      currentParagraph++;
+      textElement.style.opacity = "1";
+      typeText();
+    }
+  }
+
+  document.addEventListener("click", nextParagraph);
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Space") {
+      e.preventDefault();
+      nextParagraph();
     }
   });
 }
 
-function clearPreviousParagraphs() {
-  while (container.firstChild) {
-    container.removeChild(container.firstChild);
-  }
-}
-
-function createParagraph() {
-  const paragraph = document.createElement("p");
-  container.appendChild(paragraph);
-  return paragraph;
-}
-
-let typingInProgress = false;
-
-function typeText() {
-  textElement = createParagraph();
-  fadeOutPreviousParagraphs();
-  typingInProgress = true;
-  const typeInterval = setInterval(() => {
-    if (index < paragraphs[currentParagraph].length) {
-      const char = paragraphs[currentParagraph].charAt(index);
-      textElement.textContent += char;
-      index++;
-
-      // Check if the text overflows the container's boundaries
-      if (container.scrollHeight > container.clientHeight) {
-        clearPreviousParagraphs();
-        textElement = createParagraph();
-        textElement.textContent = char;
-      }
-    } else {
-      clearInterval(typeInterval);
-      index = 0;
-    }
-  }, 50);
-}
-
-typeText();
-
-function nextParagraph() {
-  if (typingInProgress) {
-    textElement.textContent = paragraphs[currentParagraph];
-    index = paragraphs[currentParagraph].length;
-    typingInProgress = false;
-    return;
-  }
-  if (currentParagraph < paragraphs.length - 1) {
-    currentParagraph++;
-    textElement.style.opacity = "1";
-    typeText();
-  }
-}
-
-document.addEventListener("click", nextParagraph);
-document.addEventListener("keydown", (e) => {
-  if (e.code === "Space") {
-    e.preventDefault();
-    nextParagraph();
-  }
-});
+init();
