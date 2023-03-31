b2111363076c7609366542b31378652ff89e90aa
Kiril Bekulov <kiril@bekulov.com>
2023-04-01 02:32:19 +0300

    dddd

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
-function splitTextIntoChunks(text, maxWords) {
-  const words = text.split(/\s+/);
-  const chunks = [];
+let textChunks = [];
+let currentChunkIndex = 0;
+let isTyping = false;
+let isCompleted = false;
 
-  let currentChunk = [];
-  let currentWordCount = 0;
+function typeText(text, callback) {
+  const characters = text.split('');
+  let currentCharacterIndex = 0;
 
-  words.forEach(word => {
-    const sentenceEnd = word.match(/[.!?]/);
+  isTyping = true;
+  isCompleted = false;
 
-    currentChunk.push(word);
-    currentWordCount++;
+  const intervalId = setInterval(() => {
+    const currentText = characters.slice(0, currentCharacterIndex + 1).join('');
+    const isLastCharacter = currentCharacterIndex === characters.length - 1;
 
-    if (sentenceEnd || currentWordCount >= maxWords) {
-      chunks.push(currentChunk.join(' '));
-      currentChunk = [];
-      currentWordCount = 0;
+    document.getElementById('content').textContent = currentText;
+
+    if (isLastCharacter) {
+      isTyping = false;
+      clearInterval(intervalId);
+      if (typeof callback === 'function') {
+        callback();
+      }
+    } else {
+      currentCharacterIndex++;
     }
-  });
-
-  if (currentChunk.length > 0) {
-    chunks.push(currentChunk.join(' '));
-  }
-
-  return chunks;
+  }, 50);
 }
+
+function completeText() {
+  if (isTyping && !isCompleted) {
+    isCompleted = true;
+    const currentText = document.getElementById('content').textContent;
+    document.getElementById('content').textContent = textChunks[currentChunkIndex];
+  }
+}
+
+function updateContent() {
+  if (currentChunkIndex < textChunks.length) {
+    if (isTyping) {
+      completeText();
+    } else {
+      const textChunk = textChunks[currentChunkIndex];
+      typeText(textChunk, () => {
+        currentChunkIndex++;
+      });
+    }
+  } else {
+    console.error('Chunk index is out of range');
+  }
+}
+
+// ... (fetch and splitTextIntoChunks functions remain unchanged)
 
 fetch('text.txt')
   .then(response => response.text())
@@ -34,3 +62,21 @@ fetch('text.txt')
   .catch(error => {
     console.error('Error fetching text:', error);
   });
+
+document.body.addEventListener('click', () => {
+  completeText();
+  if (!isTyping) {
+    currentChunkIndex++;
+    updateContent();
+  }
+});
+
+document.body.addEventListener('keyup', (event) => {
+  if (event.code === 'Space') {
+    completeText();
+    if (!isTyping) {
+      currentChunkIndex++;
+      updateContent();
+    }
+  }
+});
