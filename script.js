a5cb4606aa352a1c1956f9663e0a7895b67f6e32
Kiril Bekulov <kiril@bekulov.com>
2023-03-31 19:32:29 +0300

    dddd

---
+++ script.js
@@ -0,0 +1,110 @@
+const background = document.querySelector(".background");
+
+function fadeIn(element, duration) {
+  let opacity = 0;
+  const interval = 10;
+  const step = interval / (duration / 1000);
+
+  const fadeInInterval = setInterval(() => {
+    opacity += step;
+    if (opacity >= 1) {
+      clearInterval(fadeInInterval);
+      opacity = 1;
+    }
+    element.style.opacity = opacity;
+  }, interval);
+}
+
+fadeIn(background, 2000);
+
+// Fetch text from txt file
+async function fetchText() {
+  const response = await fetch("text.txt");
+  const text = await response.text();
+  return text;
+}
+
+async function init() {
+  const text = await fetchText();
+  const paragraphs = text
+    .split("\n")
+    .filter((line) => line.trim() !== "");
+
+  const container = document.querySelector(".container");
+
+  let currentParagraph = 0;
+  let index = 0;
+  let textElement;
+
+  function clearPreviousParagraphs() {
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+  }
+
+  function nextParagraph() {
+    if (typingInProgress) {
+      textElement.textContent = paragraphs[currentParagraph];
+      index = paragraphs[currentParagraph].length;
+      typingInProgress = false;
+      return;
+    }
+    if (currentParagraph < paragraphs.length - 1) {
+      currentParagraph++;
+      typeText();
+    }
+  }
+
+  function createParagraph() {
+    const paragraph = document.createElement("p");
+    container.appendChild(paragraph);
+    return paragraph;
+  }
+
+  async function fadeOutPreviousParagraph() {
+    if (currentParagraph > 0) {
+      const prevParagraph = container.children[currentParagraph - 1];
+      prevParagraph.style.transition = 'opacity 0.5s';
+      prevParagraph.style.opacity = "0.5";
+      await new Promise(resolve => setTimeout(resolve, 500)); // wait for 500ms
+    }
+  }
+
+  let typingInProgress = false;
+
+  async function typeText() {
+    await fadeOutPreviousParagraph();
+    
+    textElement = createParagraph();
+    typingInProgress = true;
+    const typeInterval = setInterval(() => {
+      if (index < paragraphs[currentParagraph].length) {
+        const char = paragraphs[currentParagraph].charAt(index);
+        textElement.textContent += char;
+        index++;
+
+        // Check if the text overflows the container's boundaries
+        if (container.scrollHeight > container.clientHeight) {
+          clearPreviousParagraphs();
+          textElement = createParagraph();
+          textElement.textContent = char;
+        }
+      } else {
+        clearInterval(typeInterval);
+        index = 0;
+      }
+    }, 50);
+  }
+
+  typeText();
+
+  document.addEventListener("click", nextParagraph);
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Space") {
+      e.preventDefault();
+      nextParagraph();
+    }
+  });
+}
+
+init();
