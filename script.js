36e211d454025f9617a179eff6fcafdf69dd02c2
Kiril Bekulov <kiril@bekulov.com>
2023-04-01 02:25:12 +0300

    ddd

--- script.js
+++ script.js
@@ -1,43 +1,21 @@
-function splitTextIntoChunks(text, maxWords) {
-  const words = text.split(/\s+/);
-  const chunks = [];
+function updateDisplayedText() {
+  if (orderNumber < textChunks.length) {
+    const contentElement = document.getElementById('content');
+    contentElement.innerHTML = '';
 
-  let currentChunk = [];
-  let currentWordCount = 0;
-
-  words.forEach(word => {
-    const sentenceEnd = word.match(/[.!?]/);
-
-    currentChunk.push(word);
-    currentWordCount++;
-
-    if (sentenceEnd || currentWordCount >= maxWords) {
-      chunks.push(currentChunk.join(' '));
-      currentChunk = [];
-      currentWordCount = 0;
-    }
-  });
-
-  if (currentChunk.length > 0) {
-    chunks.push(currentChunk.join(' '));
+    const pElement = document.createElement('p');
+    pElement.textContent = textChunks[orderNumber];
+    contentElement.appendChild(pElement);
+  } else {
+    console.error('Order number is out of range');
   }
-
-  return chunks;
 }
 
 fetch('text.txt')
   .then(response => response.text())
   .then(data => {
-    const contentElement = document.getElementById('content');
-    const textChunks = splitTextIntoChunks(data, 200);
-    
-    if (orderNumber < textChunks.length) {
-      const pElement = document.createElement('p');
-      pElement.textContent = textChunks[orderNumber];
-      contentElement.appendChild(pElement);
-    } else {
-      console.error('Order number is out of range');
-    }
+    textChunks = splitTextIntoChunks(data, 200);
+    updateDisplayedText();
   })
   .catch(error => {
     console.error('Error fetching text:', error);
