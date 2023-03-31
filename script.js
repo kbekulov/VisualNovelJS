efaeb6f06a145b59afc9623d72bee306dd1addce
Kiril Bekulov <kiril@bekulov.com>
2023-04-01 02:16:07 +0300

    jjj

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 fetch('text.txt')
   .then(response => response.text())
   .then(data => {
-    document.getElementById('content').innerHTML = data;
+    const contentElement = document.getElementById('content');
+    const paragraphs = data.split('\n').filter(line => line.trim() !== '');
+    
+    paragraphs.forEach(paragraph => {
+      const pElement = document.createElement('p');
+      pElement.textContent = paragraph;
+      contentElement.appendChild(pElement);
+    });
   })
   .catch(error => {
     console.error('Error fetching text:', error);
