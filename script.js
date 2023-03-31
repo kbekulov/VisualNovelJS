7984c29838d6c96d9ba8ed4001c73606a11ba9fa
Kiril Bekulov <kiril@bekulov.com>
2023-04-01 02:55:21 +0300

    ddd

--- script.js
+++ script.js
@@ -1,35 +1,34 @@
-function splitTextIntoChunks(text, maxWords) {
-  const words = text.split(/\s+/);
-  const chunks = [];
+const WORDS_PER_CHUNK = 200;
 
-  let currentChunk = [];
-  let currentWordCount = 0;
-
-  words.forEach(word => {
-    const sentenceEnd = word.match(/[.!?]/);
-
-    currentChunk.push(word);
-    currentWordCount++;
-
-    if (sentenceEnd || currentWordCount >= maxWords) {
-      chunks.push(currentChunk.join(' '));
-      currentChunk = [];
-      currentWordCount = 0;
-    }
-  });
-
-  if (currentChunk.length > 0) {
-    chunks.push(currentChunk.join(' '));
-  }
-
-  return chunks;
+function getQueryParam(name, defaultValue) {
+  const params = new URLSearchParams(window.location.search);
+  return params.get(name) || defaultValue;
 }
 
 fetch('text.txt')
   .then(response => response.text())
   .then(data => {
-    textChunks = splitTextIntoChunks(data, 200);
-    updateContent();
+    const contentElement = document.getElementById('content');
+    const sentences = data.match(/[^.!?]+[.!?]+/g).map(sentence => sentence.trim());
+    const chunks = [];
+    let currentChunk = '';
+
+    sentences.forEach(sentence => {
+      if ((currentChunk + ' ' + sentence).split(' ').length <= WORDS_PER_CHUNK) {
+        currentChunk += ' ' + sentence;
+      } else {
+        chunks.push(currentChunk.trim());
+        currentChunk = sentence;
+      }
+    });
+
+    if (currentChunk !== '') {
+      chunks.push(currentChunk.trim());
+    }
+
+    const order = getQueryParam('order', 1);
+    const content = chunks[(order - 1) % chunks.length];
+    contentElement.textContent = content;
   })
   .catch(error => {
     console.error('Error fetching text:', error);
