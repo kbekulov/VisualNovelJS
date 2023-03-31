6b9e38c67495498ed303f50483fe81d28c5f0a92
Kiril Bekulov <kiril@bekulov.com>
2023-04-01 02:20:00 +0300

    ddd

--- script.js
+++ script.js
@@ -1,14 +1,43 @@
+function splitTextIntoChunks(text, maxWords) {
+  const words = text.split(/\s+/);
+  const chunks = [];
+
+  let currentChunk = [];
+  let currentWordCount = 0;
+
+  words.forEach(word => {
+    const sentenceEnd = word.match(/[.!?]/);
+
+    currentChunk.push(word);
+    currentWordCount++;
+
+    if (sentenceEnd || currentWordCount >= maxWords) {
+      chunks.push(currentChunk.join(' '));
+      currentChunk = [];
+      currentWordCount = 0;
+    }
+  });
+
+  if (currentChunk.length > 0) {
+    chunks.push(currentChunk.join(' '));
+  }
+
+  return chunks;
+}
+
 fetch('text.txt')
   .then(response => response.text())
   .then(data => {
     const contentElement = document.getElementById('content');
-    const paragraphs = data.split('\n').filter(line => line.trim() !== '');
+    const textChunks = splitTextIntoChunks(data, 200);
     
-    paragraphs.forEach(paragraph => {
+    if (orderNumber < textChunks.length) {
       const pElement = document.createElement('p');
-      pElement.textContent = paragraph;
+      pElement.textContent = textChunks[orderNumber];
       contentElement.appendChild(pElement);
-    });
+    } else {
+      console.error('Order number is out of range');
+    }
   })
   .catch(error => {
     console.error('Error fetching text:', error);
