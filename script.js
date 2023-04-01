429bd3f39f74225ad63de7d4675610f3c9065f85
Kiril Bekulov <kiril@bekulov.com>
2023-04-01 03:22:11 +0300

    sss

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const WORDS_PER_CHUNK = 200;
 const TYPEWRITER_DELAY_MS = 50;
 const TYPEWRITER_END_PAUSE_MS = 1000;
+const MAX_LINES = 10;
 
 function getQueryParam(name, defaultValue) {
   const params = new URLSearchParams(window.location.search);
@@ -13,25 +13,10 @@ function updateContent(order) {
     .then(data => {
       const contentElement = document.getElementById('content');
       const sentences = data.match(/[^.!?]+[.!?]+/g).map(sentence => sentence.trim());
-      const chunks = [];
-      let currentChunk = '';
-
-      sentences.forEach(sentence => {
-        if ((currentChunk + ' ' + sentence).split(' ').length <= WORDS_PER_CHUNK) {
-          currentChunk += ' ' + sentence;
-        } else {
-          chunks.push(currentChunk.trim());
-          currentChunk = sentence;
-        }
-      });
-
-      if (currentChunk !== '') {
-        chunks.push(currentChunk.trim());
-      }
-
-      let currentChunkIndex = (order - 1) % chunks.length;
+      let currentIndex = (order - 1) % sentences.length;
+      let previousIndices = [];
       let currentText = '';
-      let currentIndex = 0;
+      let currentIndexInText = 0;
       let isTyping = true;
       let isSentenceEnd = false;
 
@@ -41,14 +26,22 @@ function updateContent(order) {
           return;
         }
 
-        if (currentIndex >= chunks[currentChunkIndex].length) {
+        if (currentIndexInText >= sentences[currentIndex].length) {
           if (isSentenceEnd) {
             isTyping = false;
             setTimeout(() => {
               isTyping = true;
               isSentenceEnd = false;
-              currentChunkIndex = (currentChunkIndex + 1) % chunks.length;
-              currentIndex = 0;
+              currentIndex = (currentIndex + 1) % sentences.length;
+              previousIndices.push(currentIndex);
+              currentIndexInText = 0;
+
+              if (previousIndices.length > MAX_LINES) {
+                const startIndex = previousIndices.length - MAX_LINES;
+                previousIndices = previousIndices.slice(startIndex);
+              }
+
+              updateDisplayedContent();
             }, TYPEWRITER_END_PAUSE_MS);
           } else {
             isSentenceEnd = true;
@@ -56,48 +49,79 @@ function updateContent(order) {
           return;
         }
 
-        currentText += chunks[currentChunkIndex][currentIndex];
-        currentIndex++;
+        currentText += sentences[currentIndex][currentIndexInText];
+        currentIndexInText++;
 
-        if (chunks[currentChunkIndex][currentIndex - 1] === '.' || chunks[currentChunkIndex][currentIndex - 1] === '?' || chunks[currentChunkIndex][currentIndex - 1] === '!') {
+        if (sentences[currentIndex][currentIndexInText - 1] === '.' || sentences[currentIndex][currentIndexInText - 1] === '?' || sentences[currentIndex][currentIndexInText - 1] === '!') {
           isSentenceEnd = true;
-        } else if (isSentenceEnd && chunks[currentChunkIndex][currentIndex - 1].match(/\w/)) {
+        } else if (isSentenceEnd && sentences[currentIndex][currentIndexInText - 1].match(/\w/)) {
           isSentenceEnd = false;
         }
 
-        contentElement.innerHTML = currentText.replace(/\n/g, '<br>');
+        updateDisplayedContent();
       }, TYPEWRITER_DELAY_MS);
 
-      document.addEventListener('click', () => {
-        if (isSentenceEnd) {
-          isTyping = true;
-          isSentenceEnd = false;
-          currentChunkIndex = (currentChunkIndex + 1) % chunks.length;
-          currentIndex = 0;
+      function updateDisplayedContent() {
+        let displayedText = '';
+        for (let i = 0; i < previousIndices.length; i++) {
+          const sentence = sentences[previousIndices[i]];
+          displayedText += sentence;
+          if (i < previousIndices.length - 1) {
+            displayedText += '<br>';
+          }
         }
-      });
+        displayedText += currentText.substring(0, currentIndexInText).replace(/\n/g, '<br>');
+        contentElement.innerHTML = displayedText;
 
-      document.addEventListener('keydown', event => {
-        if (event.code === 'Space') {
+        document.addEventListener('click', () => {
           if (isSentenceEnd) {
             isTyping = true;
             isSentenceEnd = false;
-            currentChunkIndex = (currentChunkIndex + 1) % chunks.length;
-            currentIndex = 0;
-            event.preventDefault();
+            currentIndex = (currentIndex + 1) % sentences.length;
+            previousIndices.push(currentIndex);
+            currentIndexInText = 0;
+
+            if (previousIndices.length > MAX_LINES) {
+              const startIndex = previousIndices.length - MAX_LINES;
+              previousIndices = previousIndices.slice(startIndex);
+            }
+
+            updateDisplayedContent();
           }
+        });
+
+        document.addEventListener('keydown', event => {
+          if (event.code === 'Space') {
+            if (isSentenceEnd) {
+              isTyping = true;
+              isSentenceEnd = false;
+              currentIndex = (currentIndex + 1) % sentences.length;
+              previousIndices.push(currentIndex);
+              currentIndexInText = 0;
+  
+              if (previousIndices.length > MAX_LINES) {
+                const startIndex = previousIndices.length - MAX_LINES;
+                previousIndices = previousIndices.slice(startIndex);
+              }
+  
+              updateDisplayedContent();
+              event.preventDefault();
+            }
+          });
         }
+  
+        updateDisplayedContent();
+      })
+      .catch(error => {
+        console.error('Error fetching text:', error);
       });
-    })
-    .catch(error => {
-      console.error('Error fetching text:', error);
-    });
-}
-
-function initializePage() {
-  const order = getQueryParam('order', 1);
-  window.history.replaceState({}, document.title, window.location.pathname + `?order=${order}`);
-  updateContent(order);
-}
-
-initializePage();
+  }
+  
+  function initializePage() {
+    const order = getQueryParam('order', 1);
+    window.history.replaceState({}, document.title, window.location.pathname + `?order=${order}`);
+    updateContent(order);
+  }
+  
+  initializePage();
+  
