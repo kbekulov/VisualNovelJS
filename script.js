a49ce40ee702dfeb670399aabcd27e7bd4c4aec3
Kiril Bekulov <kiril@bekulov.com>
2023-04-01 03:01:17 +0300

    mkk

--- script.js
+++ script.js
@@ -1,50 +1,59 @@
 const WORDS_PER_CHUNK = 200;
-let order = 1;
 
 function getQueryParam(name, defaultValue) {
   const params = new URLSearchParams(window.location.search);
   return params.get(name) || defaultValue;
 }
 
-document.addEventListener('click', () => {
-  order = (order % chunks.length) + 1;
-  const content = chunks[order - 1];
-  contentElement.textContent = content;
-});
+function updateContent(order) {
+  fetch('text.txt')
+    .then(response => response.text())
+    .then(data => {
+      const contentElement = document.getElementById('content');
+      const sentences = data.match(/[^.!?]+[.!?]+/g).map(sentence => sentence.trim());
+      const chunks = [];
+      let currentChunk = '';
 
-document.addEventListener('keydown', event => {
-  if (event.code === 'Space') {
-    order = (order % chunks.length) + 1;
-    const content = chunks[order - 1];
-    contentElement.textContent = content;
-  }
-});
+      sentences.forEach(sentence => {
+        if ((currentChunk + ' ' + sentence).split(' ').length <= WORDS_PER_CHUNK) {
+          currentChunk += ' ' + sentence;
+        } else {
+          chunks.push(currentChunk.trim());
+          currentChunk = sentence;
+        }
+      });
 
-fetch('text.txt')
-  .then(response => response.text())
-  .then(data => {
-    const contentElement = document.getElementById('content');
-    const sentences = data.match(/[^.!?]+[.!?]+/g).map(sentence => sentence.trim());
-    const chunks = [];
-    let currentChunk = '';
-
-    sentences.forEach(sentence => {
-      if ((currentChunk + ' ' + sentence).split(' ').length <= WORDS_PER_CHUNK) {
-        currentChunk += ' ' + sentence;
-      } else {
+      if (currentChunk !== '') {
         chunks.push(currentChunk.trim());
-        currentChunk = sentence;
       }
+
+      const content = chunks[(order - 1) % chunks.length];
+      contentElement.textContent = content;
+    })
+    .catch(error => {
+      console.error('Error fetching text:', error);
     });
+}
 
-    if (currentChunk !== '') {
-      chunks.push(currentChunk.trim());
-    }
+function initializePage() {
+  const order = getQueryParam('order', 1);
+  window.history.replaceState({}, document.title, window.location.pathname + `?order=${order}`);
+  updateContent(order);
 
-    order = parseInt(getQueryParam('order', 1));
-    const content = chunks[order - 1];
-    contentElement.textContent = content;
-  })
-  .catch(error => {
-    console.error('Error fetching text:', error);
+  document.addEventListener('click', () => {
+    const newOrder = Number(getQueryParam('order', 1)) + 1;
+    window.history.replaceState({}, document.title, window.location.pathname + `?order=${newOrder}`);
+    updateContent(newOrder);
   });
+
+  document.addEventListener('keydown', event => {
+    if (event.code === 'Space') {
+      const newOrder = Number(getQueryParam('order', 1)) + 1;
+      window.history.replaceState({}, document.title, window.location.pathname + `?order=${newOrder}`);
+      updateContent(newOrder);
+      event.preventDefault();
+    }
+  });
+}
+
+initializePage();
